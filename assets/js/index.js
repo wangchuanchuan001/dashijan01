3b95c68c04111a88d3a8ff67e1e642f7760fa751
wangchuanchuan <1060693137@qq.com>
2020-12-08 09:21:04 +0800

    完成了个人中心功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,34 @@
 
-$(function() {
-    // 调用 getUserInfo 获取用户基本信息
-    getUserInfo()
-    var layer = layui.layer
-// 3.移除本地缓存的 token，并且跳转到登录页面
-$("#btnLogout").on('click',function(){
-      // 提示用户是否确认退出
-    layer.confirm('确定退出登录?', {icon: 3, title:'提示'}, function(index){
-        //do something
-           // 1. 清空本地存储中的 token
+$(function () {
+  // 调用 getUserInfo 获取用户基本信息
+  getUserInfo()
+  var layer = layui.layer
+  // 3.移除本地缓存的 token，并且跳转到登录页面
+  $("#btnLogout").on('click', function () {
+    // 提示用户是否确认退出
+    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+      //do something
+      // 1. 清空本地存储中的 token
       localStorage.removeItem('token')
       // 2. 重新跳转到登录页面
       location.href = '/login.html'
 
-        layer.close(index);
-      });
-})
+      layer.close(index);
+    });
+  })
 
 
 
-     // 1.获取用户的基本信息
+  // 1.获取用户的基本信息
   function getUserInfo() {
     $.ajax({
       method: 'GET',
       url: '/my/userinfo',
       // headers 就是请求头配置对象
-    //   headers: {
-    //     Authorization: localStorage.getItem('token') || ''
-    //   },
-      success: function(res) {
+      //   headers: {
+      //     Authorization: localStorage.getItem('token') || ''
+      //   },
+      success: function (res) {
         if (res.status !== 0) {
           return layui.layer.msg('获取用户信息失败！')
         }
@@ -38,8 +38,8 @@ $("#btnLogout").on('click',function(){
       }
     })
   }
- // 2.渲染用户的头像
-function renderAvatar(user) {
+  // 2.渲染用户的头像
+  function renderAvatar(user) {
     // 1. 获取用户的名称
     var name = user.nickname || user.username
     // 2. 设置欢迎的文本
@@ -66,4 +66,4 @@ function renderAvatar(user) {
 
 
 
-  })
+})
