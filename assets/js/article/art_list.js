968dd9fb71196a2e0fb236f1185ad48f0cb0019e
wangchuanchuan <1060693137@qq.com>
2020-12-09 16:03:08 +0800

    完成文章管理相关功能的开发

---
+++ assets/js/article/art_list.js
@@ -0,0 +1,130 @@
+$(function () {
+    var layer = layui.layer//添加此行导入
+    var form = layui.form
+    var laypage = layui.laypage
+    // 定义一个查询的参数对象，将来请求数据的时候，
+    // 需要将请求参数对象提交到服务器
+    var q = {
+        pagenum: 1, // 页码值，默认请求第一页的数据
+        pagesize: 2, // 每页显示几条数据，默认每页显示2条
+        cate_id: '', // 文章分类的 Id
+        state: '' // 文章的发布状态
+    }
+    initTable()
+    initCate()
+    // 获取文章列表数据的方法
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章列表失败！')
+                }
+                // 使用模板引擎渲染页面的数据
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+                renderPage(res.total)
+            }
+        })
+    }
+
+    //分类的方法
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取分类数据失败！')
+                }
+                // 调用模板引擎渲染分类的可选项
+                var htmlStr = template('tpl-cate', res)
+                $('[name=cate_id]').html(htmlStr)
+                // 通过 layui里面的方法重新渲染表单区域的UI结构
+                form.render()
+            }
+        })
+    }
+    $('#form-search').on('submit', function (e) {
+        e.preventDefault()
+        // 获取表单中选中项的值
+        var cate_id = $('[name=cate_id]').val()
+        var state = $('[name=state]').val()
+        // 为查询参数对象 q 中对应的属性赋值
+        q.cate_id = cate_id
+        q.state = state
+        // 根据最新的筛选条件，重新渲染表格的数据
+        initTable()
+    })
+    function renderPage(total) {
+        laypage.render({
+            elem: 'pageBox',// 分页容器的 Id
+            count: total, // 总数据条数
+            limit: q.pagesize, // 每页显示几条数据
+            curr: q.pagenum,// 设置默认被选中的分页
+            layout: ['prev', 'limit', 'page', 'next', 'skip', 'count'],
+            limits: [2, 3, 5, 10],// 每页展示多少条
+            jump: function (obj, first) {
+                // 可以通过 first 的值，来判断是通过哪种方式，触发的 jump 回调
+                // 如果 first 的值为 true，证明是方式2触发的
+                // 否则就是方式1触发的
+                console.log(first)
+                console.log(obj.curr)
+                console.log(obj.limit);
+                // 把最新的页码值，赋值到 q 这个查询参数对象中
+                q.pagenum = obj.curr
+                q.pagesize = obj.limit
+                // 根据最新的 q 获取对应的数据列表，并渲染表格
+                // initTable()
+                if (!first) {
+                    initTable()
+                }
+            }
+        })
+    }
+
+    $('tbody').on('click', '.btn-delete', function () {
+        // 获取当前页面删除按钮的个数
+        var len = $('.btn-delete').length
+        // 获取到文章的 id
+        var id = $(this).attr('data-id')
+        // console.log('ok');
+        layer.confirm('确认删除?', { icon: 3, title: '提示' }, function (index) {
+            $.ajax({
+                mothod: 'GET',
+                url: '/my/article/delete/' + id,
+                success: function (res) {
+                    if (res.status !== 0) {
+                        return layer.msg('删除文章失败！')
+                    }
+                    layer.msg('删除文章成功！')
+                    // 当数据删除完成后，需要判断当前这一页中，是否还有剩余的数据
+                    // 如果没有剩余的数据了,则让页码值 -1 之后,
+                    // 再重新调用 initTable 方法
+                    // 4
+                    if (len === 1) {
+                        // 如果 len 的值等于1，证明删除完毕之后，页面上就没有任何数据了
+                        // 页码值最小必须是 1
+                        q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1
+                    }
+                    initTable()
+                }
+            })
+
+            layer.close(index);
+        });
+    })
+    // 编辑功能
+    var indexEdit = null
+    $('tbody').on('click', '.btn-edit', function () {
+        // 弹出一个修改文章分类信息的层
+        indexEdit = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '修改文章分类',
+            content: $('#dialog-edit').html()
+        })
+    })
+})
